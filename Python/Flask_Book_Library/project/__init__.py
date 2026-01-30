bfc283302eed62449c844c1659d702d5e9e21266
Tomasz Wężowicz <t.wezowicz7@gmail.com>
2026-01-30 22:08:28 +0100

    Remove secret.

--- Python/Flask_Book_Library/project/__init__.py
+++ Python/Flask_Book_Library/project/__init__.py
@@ -6,13 +6,13 @@ from flask_migrate import Migrate
 
 # Database Setup
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'supersecret' # To allow us to use forms
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "data.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
